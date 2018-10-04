b9ed05d5a829eb371f1ee13f53c86811febc8485
Pipat Waitayaworanart <coploftbas@gmail.com>
2018-10-04 21:58:28 +0700

    React not vue

--- src/components/Header.react.js
+++ src/components/Header.react.js
@@ -5,7 +5,7 @@ class Header extends React.Component {
     return (
       <div className="section">
         <h1><a className="title is-1" href="#">CopLoftBas - ReactJS TH</a></h1>
-        <h2 className="subtitle is-3">Playing with ReactJS</h2>
+        <h2 className="subtitle is-3">Playing with ReactJS not VUE</h2>
       </div>
     )
   }
